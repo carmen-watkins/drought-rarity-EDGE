7582a938d338e8cccb07fae215ec9ba019cd55ae
Carmen Watkins <cebel2@uoregon.edu>
2023-06-22 13:36:38 -0400

    change RR to (D-C)/(D+C)

--- analyses/calculate_response_ratio.R
+++ analyses/calculate_response_ratio.R
@@ -6,7 +6,7 @@ source("data-prep/clean_edge_data.R")
 # Calcs ####
 ## Resp Ratio Across Years ####
 ### During Drought ####
-## (control - treatment)/control + drought
+## (drought - control)/control + drought
 #### Block Level ####
 resp.ratio.block <- edge_all %>%
   filter(experiment.year %in% c(1:4)) %>% ## 0 is pre-treat year; drought was years 1-4
@@ -15,7 +15,7 @@ resp.ratio.block <- edge_all %>%
   pivot_wider(names_from = "treatment", values_from = "mean.cover.sp") %>%
   ungroup() %>%
   group_by(site, block, species) %>%
-  mutate(resp.ratio.block = (C-D)/(C+D)) 
+  mutate(resp.ratio.block = (D-C)/(C+D)) 
 
 ## merge with rank and persistence values for each species
 edge_w_predictors.block <- left_join(resp.ratio.block, rank_persist, by = c("site", "species"))
@@ -33,7 +33,7 @@ resp.ratio.site <- edge_all %>%
   pivot_wider(names_from = "treatment", values_from = "mean.cover.sp") %>%
   ungroup() %>%
   group_by(site, species) %>%
-  mutate(resp.ratio.site = (C-D)/(C+D)) 
+  mutate(resp.ratio.site = (D-C)/(C+D)) 
 
 ## merge with rank and persistence values for each species
 edge_w_predictors.site <- left_join(resp.ratio.site, rank_persist, by = c("site", "species"))
@@ -44,6 +44,23 @@ edge_w_predictors.site <- edge_w_predictors.site %>%
   mutate(site = fct_relevel(site, c("KNZ", "HYS", "CHY", "SGS", "SEV_blue", "SEV_black")))
 
 ### During Recovery ####
+#### Site Level ####
+resp.ratio.site.recov <- edge_all %>%
+  filter(treatment.year == "recovery") %>% 
+  group_by(site, treatment, species) %>%
+  summarise(mean.cover.sp = mean(mean.plot.cover)) %>% ## mean cover by site across years
+  pivot_wider(names_from = "treatment", values_from = "mean.cover.sp") %>%
+  ungroup() %>%
+  group_by(site, species) %>%
+  mutate(resp.ratio.site = (D-C)/(C+D))
+
+## merge with rank and persistence values for each species
+edge_w_predictors.site.recov <- left_join(resp.ratio.site.recov, rank_persist, by = c("site", "species"))
+
+## change site to an ordered factor
+edge_w_predictors.site.recov$site <- as.factor(edge_w_predictors.site.recov$site)
+edge_w_predictors.site.recov <- edge_w_predictors.site.recov %>%
+  mutate(site = fct_relevel(site, c("KNZ", "HYS", "CHY", "SGS", "SEV_blue", "SEV_black")))
 
 
 
@@ -56,10 +73,17 @@ resp.ratio.yearly <- edge_all %>%
   pivot_wider(names_from = "treatment", values_from = "mean.cover.sp") %>%
   ungroup() %>%
   group_by(site, species, experiment.year, treatment.year) %>%
-  mutate(resp.ratio.site = (C-D)/(C+D))
+  mutate(resp.ratio.site = (D-C)/(C+D))
 
 edge_yearly_w_predictors <- left_join(resp.ratio.yearly, rank_persist, by = c("site", "species"))
 
 edge_yearly_w_predictors$site <- as.factor(edge_yearly_w_predictors$site)
 edge_yearly_w_predictors <- edge_yearly_w_predictors %>%
   mutate(site = fct_relevel(site, c("KNZ", "HYS", "CHY", "SGS", "SEV_blue", "SEV_black")))
+
+year.key <- edge_all %>%
+  group_by(site, experiment.year, treatment.year, year) %>%
+  summarise(year2 = unique(year)) 
+
+
+
