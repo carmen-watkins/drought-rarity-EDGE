ce7a6699d619d28def85d46f2e0617147ae17918
Carmen Watkins <cebel2@uoregon.edu>
2023-07-28 07:54:58 -0700

    tweak figs/explore richness v. slope

--- analyses/response_ratio_drought_recovery.R
+++ analyses/response_ratio_drought_recovery.R
@@ -123,33 +123,85 @@ slope.DR <- ggplot(RR.slope.map, aes(x=MAP.mm, y=slope.drought.rank)) +
   scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
   xlab("MAP (mm)") +
   ylab("Slope (DRR v. Rank)") +
-  geom_smooth(method = "lm", alpha = 0.15, color = "black")
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,1))
 
 slope.RR <- ggplot(RR.slope.map, aes(x=MAP.mm, y=slope.recov.rank)) +
   geom_point(size = 3, aes(color = site)) +
   scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
   xlab("MAP (mm)") +
   ylab("Slope (RRR vs. Rank)") +
-  geom_smooth(method = "lm", alpha = 0.15, color = "black")
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,1))
 
 slope.DP <- ggplot(RR.slope.map, aes(x=MAP.mm, y=slope.drought.persistence)) +
   geom_point(size = 3, aes(color = site)) +
   scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
   xlab("MAP (mm)") +
   ylab("Slope (DRR vs. Persist)") +
-  geom_smooth(method = "lm", alpha = 0.15, color = "black")
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,1))
 
 slope.RP <- ggplot(RR.slope.map, aes(x=MAP.mm, y=slope.recov.persistence)) +
   geom_point(size = 3, aes(color = site)) +
   scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
   xlab("MAP (mm)") +
   ylab("Slope (RRR vs. Persist)") +
-  geom_smooth(method = "lm", alpha = 0.15, color = "black")
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,1))
 
 ggarrange(slope.DR, slope.DP, slope.RR, slope.RP, labels = "AUTO", common.legend = T, 
           legend = "bottom")
 
-#ggsave("preliminary_figs/resp_ratio_rank_persistence/slopes_vs_MAP.png", width = 5, height = 5.5)
+ggsave("preliminary_figs/resp_ratio_rank_persistence/slopes_vs_MAP.png", width = 5, height = 5.5)
+
+## RR Slope v. Site Rich ####
+rich <- edge_all %>%
+  group_by(site) %>%
+  summarize(SR=length(unique(species,na.rm=T)))
+
+
+RR.slope.rich <- left_join(RR.slope.map, rich, by = c("site"))
+
+### Visualize ####
+slope.DR.rich <- ggplot(RR.slope.rich, aes(x=SR, y=slope.drought.rank)) +
+  geom_point(size = 3, aes(color = site)) +
+  scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
+  xlab("Richness") +
+  ylab("Slope (DRR v. Rank)") +
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,0.75))
+
+slope.RR.rich <- ggplot(RR.slope.rich, aes(x=SR, y=slope.recov.rank)) +
+  geom_point(size = 3, aes(color = site)) +
+  scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
+  xlab("Richness") +
+  ylab("Slope (RRR vs. Rank)") +
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,0.75))
+
+slope.DP.rich <- ggplot(RR.slope.rich, aes(x=SR, y=slope.drought.persistence)) +
+  geom_point(size = 3, aes(color = site)) +
+  scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
+  xlab("Richness") +
+  ylab("Slope (DRR vs. Persist)") +
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,0.75))
+
+slope.RP.rich <- ggplot(RR.slope.rich, aes(x=SR, y=slope.recov.persistence)) +
+  geom_point(size = 3, aes(color = site)) +
+  scale_color_manual(values = c("#008080", "#70a494", "#b4c8a8", "#edbb8a","#de8a5a", "#ca562c")) +
+  xlab("Richness") +
+  ylab("Slope (RRR vs. Persist)") +
+  geom_smooth(method = "lm", alpha = 0.15, color = "black") +
+  coord_cartesian(ylim = c(-2,0.75))
+
+ggarrange(slope.DR.rich, slope.DP.rich, slope.RR.rich, slope.RP.rich, labels = "AUTO", common.legend = T, 
+          legend = "bottom")
+
+ggsave("preliminary_figs/resp_ratio_rank_persistence/slopes_vs_rich.png", width = 5, height = 5.5)
+
+
 
 ## RR v. Rank/Persist ####
 ### During Drought ####
