4e5a92388e23dca53e48919a0076ac0be233ec67
Carmen Watkins <cebel2@uoregon.edu>
2024-01-29 10:48:12 -0800

    Final changes

---
+++ analyses/FG_models.R
@@ -0,0 +1,30 @@
+library(lmerTest)
+
+## rank & persistence in same model
+## drought
+FG_model<-lm(resp.ratio.site~persistence.site*FunctionalGroup*percrank*site, data = edge_FG[edge_FG$FunctionalGroup != "tree" & !is.na(edge_FG$FunctionalGroup),])
+
+summary(FG_model)
+anova(FG_model)
+
+## recovery
+FG_model_recov<-lm(resp.ratio.site~persistence.site*FunctionalGroup*percrank*site, data = edge_FG_recov[edge_FG_recov$FunctionalGroup != "tree" & !is.na(edge_FG_recov$FunctionalGroup),])
+
+summary(FG_model_recov)
+anova(FG_model_recov)
+
+
+## rank only in models
+FG_modelR<-lm(resp.ratio.site~FunctionalGroup*percrank*site, data = edge_FG[edge_FG$FunctionalGroup != "tree" & !is.na(edge_FG$FunctionalGroup),])
+
+summary(FG_modelR)
+anova(FG_modelR)
+
+
+FG_model_recovR<-lm(resp.ratio.site~FunctionalGroup*percrank, data = edge_FG_recov[edge_FG_recov$FunctionalGroup != "tree" & !is.na(edge_FG_recov$FunctionalGroup),])
+
+summary(FG_model_recovR)
+
+
+
+## site as random effect?
--- analyses/response_ratio_funct_group.R
+++ analyses/response_ratio_funct_group.R
@@ -1,6 +1,5 @@
 
 source("analyses/calculate_response_ratio.R") 
-#source("data-prep/funct_group_key.R")
 FG <- read.csv("data/edge_species_info.csv")
 
 theme_set(theme_classic())
@@ -18,7 +17,114 @@ edge_temp2 <- edge_w_predictors.site.recov %>%
 
 edge_FG_recov <- left_join(edge_temp2, FG, by = "species")
 
-# Overall FG Patterns ####
+# Fig 2: sp strategies ####
+## Figure ####
+drought.RR <- edge_FG %>%
+  mutate(resp.ratio.drought = resp.ratio.site,
+         mean.cov.drought = mean.cov) %>%
+  select(site, species, resp.ratio.drought, mean.cov.drought, persistence.site, percrank, FunctionalGroup)
+
+recov.RR <- edge_FG_recov %>%
+  mutate(resp.ratio.recov = resp.ratio.site,
+         mean.cov.recov = mean.cov) %>%
+  select(site, species, resp.ratio.recov, mean.cov.recov, FunctionalGroup)
+
+## merge drought & recov dataframes
+response.ratio.tog <- left_join(drought.RR, recov.RR, by = c("site", "species", "FunctionalGroup")) %>%
+  mutate(precip.bin = ifelse(site %in% c("KNZ", "HYS"), "high",
+                             ifelse(site %in% c("CHY", "SGS"), "med", "low")))
+
+response.ratio.tog$precip.bin <- as.factor(response.ratio.tog$precip.bin)
+
+response.ratio.tog <- response.ratio.tog %>%
+  mutate(precip.bin = fct_relevel(precip.bin, "high", "med", "low"))
+
+## visualize
+ggplot(response.ratio.tog, aes(x=resp.ratio.drought, resp.ratio.recov, color = FunctionalGroup)) +
+  geom_hline(yintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_vline(xintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_point(shape = 20, size = 2) +
+  facet_wrap(~site, nrow = 3, ncol = 2) +
+  #geom_smooth(method = "lm", alpha = 0.10, color = "black", linewidth = 0.75) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  xlab("Drought Response Ratio") +
+  ylab("Recovery Response Ratio") +
+  scale_color_manual(values = c("#CC61B0", "#99C945","#5D69B1", "#E58606"))
+
+ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig2_DRR_v_RRR_FG.png", width = 5, height = 4.5)
+
+## Precip Version ####
+ggplot(response.ratio.tog, aes(x=resp.ratio.drought, resp.ratio.recov, color = precip.bin)) +
+  geom_hline(yintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_vline(xintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_point(shape = 20, size = 2) +
+  facet_wrap(~precip.bin, nrow = 1, ncol = 3) +
+  #geom_smooth(method = "lm", alpha = 0.10, color = "black", linewidth = 0.75) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  xlab("Drought Response Ratio") +
+  ylab("Recovery Response Ratio") +
+  scale_color_manual(values = c("#42B7B9","#ca562c", "#D691C1")) +
+  labs(color="Relative PPT Bin")
+  
+ggsave("preliminary_figs/resp_ratio_rank_persistence/DRR_v_RRR_precip_bin.png", width = 7, height = 2.5)
+
+
+## Summary Stats ####
+sumRR <- response.ratio.tog %>% 
+  mutate(quad = ifelse(resp.ratio.drought > 0 & resp.ratio.recov > 0, "Q2",
+                          ifelse(resp.ratio.drought > 0 & resp.ratio.recov < 0, "Q4",
+                                 ifelse(resp.ratio.drought < 0 & resp.ratio.recov < 0, "Q3",
+                                        ifelse(resp.ratio.drought < 0 & resp.ratio.recov > 0,"Q1", NA))))) %>%
+  group_by(site, quad) %>%
+  summarise(quad.tot = n()) %>%
+  ungroup() %>%
+  group_by(site) %>%
+  mutate(overall.tot= sum(quad.tot),
+         percent.quad = quad.tot/overall.tot)
+
+sumRR.ppt <- response.ratio.tog %>% 
+  mutate(quad = ifelse(resp.ratio.drought > 0 & resp.ratio.recov > 0, "Q2",
+                       ifelse(resp.ratio.drought > 0 & resp.ratio.recov < 0, "Q4",
+                              ifelse(resp.ratio.drought < 0 & resp.ratio.recov < 0, "Q3",
+                                     ifelse(resp.ratio.drought < 0 & resp.ratio.recov > 0,"Q1", NA))))) %>%
+  group_by(precip.bin, quad) %>%
+  summarise(quad.tot = n()) %>%
+  ungroup() %>%
+  group_by(precip.bin) %>%
+  mutate(overall.tot= sum(quad.tot),
+         percent.quad = quad.tot/overall.tot)
+
+
+ggplot(sumRR, aes(x=quad, y=percent.quad)) +
+  geom_bar(stat = 'identity') +
+  facet_wrap(~site, ncol = 2, nrow = 3) +
+  xlab("Quadrant") + ylab("% Species of Total")
+
+ggsave("preliminary_figs/sp_percent_quadrants_site.png", height = 5, width = 5)
+
+ggplot(sumRR.ppt, aes(x=quad, y=percent.quad)) +
+  geom_bar(stat = 'identity') +
+  facet_wrap(~precip.bin, ncol = 3, nrow = 1) +
+  xlab("Quadrant") + ylab("% Species of Total")
+
+ggsave("preliminary_figs/sp_percent_quadrants_ppt.png", height = 2, width = 5)
+
+## Precip Facet x FG color ####
+ggplot(response.ratio.tog, aes(x=resp.ratio.drought, resp.ratio.recov, color = FunctionalGroup)) +
+  geom_hline(yintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_vline(xintercept = 0, color = "grey", linewidth = 0.25) +
+  geom_point(shape = 20, size = 2) +
+  facet_wrap(~precip.bin, nrow = 1, ncol = 3) +
+  #geom_smooth(method = "lm", alpha = 0.10, color = "black", linewidth = 0.75) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  xlab("Drought Response Ratio") +
+  ylab("Recovery Response Ratio") +
+  scale_color_manual(values = c("#CC61B0", "#99C945","#5D69B1", "#E58606")) +
+  labs(color="Functional Group")
+
+ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig2_DRR_v_RRR_facetppt_colorFG.png", width = 7, height = 2.5)
+
+# Figure 3: Overall FG patterns ####
 FGrankD <- ggplot(edge_FG, aes(x=percrank, y=resp.ratio.site)) +
   geom_point(aes(color = FunctionalGroup), alpha = 0.9, size = 0.9) +
   geom_smooth(aes(color = FunctionalGroup), method = "lm", alpha = 0, linewidth = 0.8) +
@@ -63,7 +169,8 @@ ggarrange(FGrankD, FGpersD, FGrankR, FGpersR, labels = "AUTO", common.legend = T
 
 ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig3_RR_drought_recov_FG.png", width = 6.5, height = 6.5)
 
-# Site Level Patterns ####
+# Other Analyses ####
+## Site Level Patterns ####
 ggplot(edge_FG, aes(x=percrank, y=resp.ratio.site)) +
   geom_point(aes(color = FunctionalGroup), alpha = 0.9, size = 0.9) +
   geom_smooth(aes(color = FunctionalGroup), method = "lm", alpha = 0, linewidth = 0.8) +
@@ -89,7 +196,6 @@ ggplot(edge_FG_recov, aes(x=percrank, y=resp.ratio.site)) +
   facet_wrap(~site)
 
 ggsave("preliminary_figs/resp_ratio_rank_persistence/RR_FG_by_site.png", width = 7, height = 3.5)
-
 
 ggplot(edge_FG, aes(x=persistence.site, y=resp.ratio.site)) +
   geom_point(aes(color = FunctionalGroup), alpha = 0.9, size = 0.9) +
@@ -118,7 +224,7 @@ ggplot(edge_FG_recov, aes(x=persistence.site, y=resp.ratio.site)) +
 ggsave("preliminary_figs/resp_ratio_rank_persistence/RP_FG_by_site.png", width = 7, height = 3.5)
 
 
-# Facet by FG
+## Facet by FG #####
 RD <- ggplot(edge_FG[edge_FG$FunctionalGroup != "tree" & !is.na(edge_FG$FunctionalGroup),], aes(x=percrank, y=resp.ratio.site)) +
   geom_point(aes(color = FunctionalGroup), alpha = 0.9, size = 0.9) +
   #geom_smooth(aes(color = FunctionalGroup), method = "lm", alpha = 0, linewidth = 0.8) +
@@ -144,7 +250,6 @@ RR <- ggplot(edge_FG_recov[edge_FG_recov$FunctionalGroup != "tree" & !is.na(edge
 ggarrange(RD, RR, ncol = 1, nrow = 2,common.legend = T, legend = "bottom")
 
 ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig4_RR_rank_FG_facet.png", width = 5.5, height = 4)
-
 
 PD <- ggplot(edge_FG[edge_FG$FunctionalGroup != "tree" & !is.na(edge_FG$FunctionalGroup),], aes(x=persistence.site, y=resp.ratio.site)) +
   geom_point(aes(color = FunctionalGroup), alpha = 0.9, size = 0.9) +
@@ -172,33 +277,6 @@ ggarrange(PD, PR, ncol = 1, nrow = 2,common.legend = T, legend = "bottom")
 
 ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig5_RP_rank_FG_facet.png", width = 5.5, height = 4)
 
-# Fig 2: sp strategies ####
-drought.RR <- edge_FG %>%
-  mutate(resp.ratio.drought = resp.ratio.site,
-         mean.cov.drought = mean.cov) %>%
-  select(site, species, resp.ratio.drought, mean.cov.drought, persistence.site, percrank, FunctionalGroup)
-
-recov.RR <- edge_FG_recov %>%
-  mutate(resp.ratio.recov = resp.ratio.site,
-         mean.cov.recov = mean.cov) %>%
-  select(site, species, resp.ratio.recov, mean.cov.recov, FunctionalGroup)
-
-## merge drought & recov dataframes
-response.ratio.tog <- left_join(drought.RR, recov.RR, by = c("site", "species", "FunctionalGroup"))
-
-## visualize
-ggplot(response.ratio.tog, aes(x=resp.ratio.drought, resp.ratio.recov, color = FunctionalGroup)) +
-  geom_hline(yintercept = 0, color = "grey", linewidth = 0.25) +
-  geom_vline(xintercept = 0, color = "grey", linewidth = 0.25) +
-  geom_point(shape = 20, size = 2) +
-  facet_wrap(~site, nrow = 3, ncol = 2) +
-  #geom_smooth(method = "lm", alpha = 0.10, color = "black", linewidth = 0.75) +
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-  xlab("Drought Response Ratio") +
-  ylab("Recovery Response Ratio") +
-  scale_color_manual(values = c("#CC61B0", "#99C945","#5D69B1", "#E58606"))
-  
-ggsave("preliminary_figs/resp_ratio_rank_persistence/Fig2_DRR_v_RRR_FG.png", width = 5, height = 4.5)
 
 
 
