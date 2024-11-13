0e0c5a7d6c311b3d331d42eafccbc556b68eaff3
baguirre6 <baguirre9393@gmail.com>
2024-11-13 10:00:38 -0800

    removed weird unnecessary grouping; and updated working dataframe to include species life history (duration)

--- data/Beatriz_Nov2024.R
+++ data/Beatriz_Nov2024.R
@@ -1,30 +1,20 @@
+# Figure 2 by Functinal Group and Life History
+# November 13, 2024
+# BAA
 
 source("analyses/calculate_response_ratio.R")
 source("analyses/color_palettes.R")
 source("analyses/response_ratio_analyses.R")
-source("analyses/old_analyses/funct_group_summaries.R")
 
 #Read in functional group info
 FG <- read.csv(here::here("data","edge_species_info_CP_BA.csv"))
 
-#add functional group to species
-sp.list.FG <- left_join(sp.list, FG, by = c("species"))
-
-sp.list.FG %>%
-  group_by(species) %>%
-  select(species, FunctionalGroup, Duration) -> sp.list.FG
-
-# Remove duplicate rows based on species
-sp.list.FG <- sp.list.FG %>%
-  distinct(species, .keep_all = TRUE)
-
-
+#Join functional group data to species response ratio data
 edge_RR2 <- edge_RR %>%
-  left_join(sp.list.FG, by = "species")
-
+  left_join(FG, by = "species")
 
 sum_edge_RR2 = edge_RR2 %>%
-  group_by(site, species, FunctionalGroup) %>%
+  group_by(site, species, FunctionalGroup, Duration) %>%
   summarise(meanDRR = mean(resp.ratio.site_D, na.rm = T), 
             meanPDRR = mean(resp.ratio.site_PD, na.rm = T),
             seDRR = calcSE(resp.ratio.site_D),
@@ -37,7 +27,6 @@ sum_edge_RR2 = edge_RR2 %>%
          rarity_cat = paste0(temporal, ", ", spatial),
          MAP_level = ifelse(site %in% c("KNZ", "HYS"), "High", 
                             ifelse(site %in% c("CHY", "SGS"), "Intermediate", "Low"))) 
-
 
 # Figure 2 with functional groups
 ggplot(sum_edge_RR2, aes(x = rank, y=persistence))+
@@ -96,24 +85,12 @@ sum_edge_RR2 %>%
 object3
 #ggsave("~/Documents/NCEAS_Transitions/subordinate-species/Nov 2024 Meeting Figures/FG_TemporalRarity_Density.jpeg", width = 12, height = 5)
 
-# Life History 
-life.history = edge_RR2 %>%
-  group_by(site, species, Duration) %>%
-  summarise(meanDRR = mean(resp.ratio.site_D, na.rm = T), 
-            meanPDRR = mean(resp.ratio.site_PD, na.rm = T),
-            seDRR = calcSE(resp.ratio.site_D),
-            sePDRR = calcSE(resp.ratio.site_PD),
-            persistence = median(persistence.site), 
-            rank = median(percrank)) %>%
-  
-  mutate(spatial = ifelse(rank > 0.75, "Abundant", "Scarce"),
-         temporal = ifelse(persistence > 0.5, "Core", "Transient"),
-         rarity_cat = paste0(temporal, ", ", spatial),
-         MAP_level = ifelse(site %in% c("KNZ", "HYS"), "High", 
-                            ifelse(site %in% c("CHY", "SGS"), "Intermediate", "Low"))) 
 
+###############################
+# Life History Figures
+###############################
 
-life.history %>%
+sum_edge_RR2 %>%
   group_by(site) %>%
   ggplot(aes(x=persistence, fill=Duration, colour = Duration)) +
   geom_density(alpha=0.05) +
@@ -129,7 +106,7 @@ life.history %>%
 figure4
 
 
-life.history %>%
+sum_edge_RR2 %>%
   group_by(site) %>%
   ggplot(aes(x=rank, fill=Duration, colour = Duration)) +
   geom_density(alpha=0.05) +
@@ -145,7 +122,7 @@ life.history %>%
 figure5
 
 
-ggplot(life.history, aes(x = rank, y=persistence))+
+ggplot(sum_edge_RR2, aes(x = rank, y=persistence))+
   geom_hline(yintercept = 0.5, color = "red", linetype = "dashed") +
   #geom_vline(xintercept = 0.5, color = "lightgray") +
   geom_vline(xintercept = 0.75, color = "red", linetype = "dashed") +
