cc262e08c5f53ea40a4686673b18b586d1875cbf
Carmen Watkins <cebel2@uoregon.edu>
2025-06-11 14:29:07 -0700

    run models for 7-yr drought

--- analyses/supplementary_analyses/FigS4_d4_vs_d7_response_ratio.R
+++ analyses/supplementary_analyses/FigS4_d4_vs_d7_response_ratio.R
@@ -5,6 +5,15 @@
 source("analyses/calc_response_ratio.R") 
 source("analyses/color_palettes.R")
 
+## load packages
+library(broom)
+library(performance)
+library(parameters)
+library(tidyverse)
+library(car)
+library(jtools)
+library(xtable)
+
 library(ggpubr)
 
 theme_set(theme_classic())
@@ -37,5 +46,72 @@ TR_d = ggplot(edge_RR[edge_RR$site %in% c("SBK", "SBL"),], aes(x=temporal_rarity
 ggarrange(SR_d, TR_d, labels = "AUTO", common.legend = T, legend = "bottom")
 
 #ggsave("figures/Mar2025/FigS4_resp_ratio_v_rarity.png", width = 6, height = 3.5)
+
+# Model ####
+## spatial, drought ####
+sites = c("SBL", "SBK")
+
+mod_df = data.frame(term = NA, estimate = NA, std.error = NA, statistic = NA, p.value = NA,  conf.low = NA, conf.high = NA, site = NA, period = NA)
+
+for(i in 1:length(sites)) {
+  
+  ## select site
+  s = sites[i]
+  
+  ## run the model
+  tmp = edge_RR[edge_RR$site == s,] %>% 
+    lm(resp.ratio.site_D4 ~ spatial_rarity, data = .) %>% 
+    tidy(conf.int = TRUE) %>%
+    mutate(site = s, 
+           period = "Drought")
+  
+  ## append
+  mod_df = rbind(mod_df, tmp) %>%
+    filter(!is.na(term))
+  
+}
+
+
+## temporal, drought ####
+modt_df = data.frame(term = NA, estimate = NA, std.error = NA, statistic = NA, p.value = NA,  conf.low = NA, conf.high = NA, site = NA, period = NA)
+
+for(i in 1:length(sites)) {
+  
+  ## select site
+  s = sites[i]
+  
+  ## run the model
+  tmp = edge_RR[edge_RR$site == s,] %>% 
+    lm(resp.ratio.site_D4 ~ temporal_rarity, data = .) %>% 
+    tidy(conf.int = TRUE) %>%
+    mutate(site = s, 
+           period = "Drought")
+  
+  ## append
+  modt_df = rbind(modt_df, tmp) %>%
+    filter(!is.na(term))
+  
+}
+
+
+sp_mods_tab = mod_df %>%
+  select(period, site, term, estimate, std.error, statistic, p.value) %>%
+  mutate(signif = ifelse(p.value < 0.001, "***", 
+                         ifelse(p.value < 0.01 & p.value > 0.001, "**",
+                                ifelse(p.value > 0.01 & p.value < 0.05, "*", 
+                                       ifelse(p.value < 0.1 & p.value > 0.05, ".", " "))))) %>%
+  mutate_if(is.numeric, round, digits = 3)
+write.csv(sp_mods_tab, "tables/site_model_output_spatial_SA_7yrdrought.csv")
+
+
+tmp_mods_tab = modt_df %>%
+  select(period, site, term, estimate, std.error, statistic, p.value) %>%
+  mutate(signif = ifelse(p.value < 0.001, "***", 
+                         ifelse(p.value < 0.01 & p.value > 0.001, "**",
+                                ifelse(p.value > 0.01 & p.value < 0.05, "*", 
+                                       ifelse(p.value < 0.1 & p.value > 0.05, ".", " "))))) %>%
+  mutate_if(is.numeric, round, digits = 3)
+write.csv(tmp_mods_tab, "tables/site_model_output_temporal_SA_7yrdrought.csv")
+
 
 
