63306a254f4ffc5caaa526f769b3cd763d4d5263
Carmen Watkins <carmenrose4@gmail.com>
2026-02-03 22:03:18 +0000

    add stats to split post-drought period figure.

--- analyses/supplementary_analyses/sensitivity_analyses/assumption_3/Q1B_split_post_drought_models.R
+++ analyses/supplementary_analyses/sensitivity_analyses/assumption_3/Q1B_split_post_drought_models.R
@@ -14,6 +14,8 @@ library(car)
 library(lmerTest)
 library(jtools)
 library(xtable)
+library(MuMIn)
+library(ggpubr)
 
 source("analyses/calc_response_ratio.R") 
 #source("analyses/color_palettes.R")
@@ -23,21 +25,26 @@ edge_PDlong = edge_RR %>%
   select(site, species, resp.ratio.site_PDfirst, resp.ratio.site_PDfinal, 
          spatial_rarity, temporal_rarity) %>%
   pivot_longer(cols = c("resp.ratio.site_PDfirst", "resp.ratio.site_PDfinal"), 
-               names_to = "PD_period", values_to = "resp.ratio")
+               names_to = "PD_period", values_to = "resp.ratio") %>%
+  mutate(PD_period = ifelse(PD_period == "resp.ratio.site_PDfirst", "initial", "final"))
 
 ## spatial rarity model
 mmpdss = lmer(resp.ratio ~ spatial_rarity + PD_period + (1|site), 
               data = edge_PDlong)
 summary(mmpdss)
 
-Anova(mmpdss)
+Anova(mmpdss, type = 2, test.statistic = "F")
+
+r.squaredGLMM(mmpdss)
+
 
 ## temporal rarity model
 mmpdts = lmer(resp.ratio ~ temporal_rarity + PD_period + (1|site), 
               data = edge_PDlong)
 summary(mmpdts)
 
-Anova(mmpdts)
+Anova(mmpdts, type = 2, test.statistic = "F")
+r.squaredGLMM(mmpdts)
 
 ## Table ####
 ## put model outputs into a table
@@ -55,3 +62,40 @@ pd_anova_df = rbind(mmpdss_tab, mmpdts_tab) %>%
 ## save table
 #write.csv(pd_anova_df, "tables/final_tables/pd_final_initial_mixed_mod_anova_table.csv",
           #row.names = F)
+
+# Figure S7 ####
+pds = edge_PDlong %>%
+  ggplot(aes(x=spatial_rarity, y=resp.ratio, color = PD_period)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  geom_hline(yintercept = 0, linetype = "dashed")  +
+  ylab("Post-Drought") +
+  xlab("Spatial Rarity") +
+  labs(color = "PD Period") +
+  theme(text = element_text(size = 13)) +
+  scale_color_manual(values = c("#855C75","#D9AF6B")) +
+  coord_cartesian(ylim = c(-1,1.2)) +
+  annotate("text", x = 0.1, y=1.16, label = "R[m]^2: 0.09", size = 3, parse = TRUE) +
+  annotate("text", x = 0.4, y=1.16, label = "R[c]^2: 0.14", size = 3, parse = TRUE)
+
+pdt = edge_PDlong %>%
+  ggplot(aes(x=spatial_rarity, y=resp.ratio, color = PD_period)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  geom_hline(yintercept = 0, linetype = "dashed")  +
+  ylab("Post-Drought") +
+  xlab("Temporal Rarity") +
+  labs(color = "PD Period") +
+  theme(text = element_text(size = 13)) +
+  scale_color_manual(values = c("#855C75","#D9AF6B")) +
+  coord_cartesian(ylim = c(-1,1.2)) +
+  annotate("text", x = 0.1, y=1.16, label = "R[m]^2: 0.10", size = 3, parse = TRUE) +
+  annotate("text", x = 0.4, y=1.16, label = "R[c]^2: 0.12", size = 3, parse = TRUE)
+
+ggarrange(pds, pdt, common.legend = T, labels = c("(a)", "(b)"), legend = "bottom")
+
+ggsave("figures/review_figs/FigS7_split_post_drought.tiff", width = 6, height = 3.5)
+
+
+"#855C75","#D9AF6B","#AF6458","#736F4C","#526A83",#625377,#68855C,#9C9C5E,#A06177,#8C785D,#467378,#7C7C7C
+
